2d6ce15e4311e577f60ee6f13fd75a9194cf58e7
vishalsg42 <vishalsg42@gmail.com>
2020-12-07 02:15:44 +0530

    removing redis implementation

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const responseTime = require('response-time');
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 const dotenvFlow = require('dotenv-flow');
-const client = require('redis').createClient();
 const cors = require('cors');
 const passport = require('passport');
 
@@ -103,14 +102,8 @@ app.use(require('express-status-monitor')());
  */
 app.use(compression());
 
-/* API rate limit configuration. */
-const limiter = require('express-limiter')(app, client);
-const apiRateLimit = require('./src/services/apiRateLimit').rateLimit;
-const limitCount = process.env.RATE_LIMIT_COUNT || 10,
-  limitMinute = process.env.RATE_LIMIT_MINUTE || 1;
-
 /* Configuring Routes */
-app.use('/api', apiRateLimit(limiter, limitCount, limitMinute), routes);
+app.use('/api',routes);
 
 /* Handling invalid route */
 app.use('/', function (req, res) {
