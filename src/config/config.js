70ad910d0a4b29b4c9a7f610bdc4cfe1ae29fdab
vishalsg42 <vishalsg42@gmail.com>
2020-12-13 16:22:34 +0530

    changed package name

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ let config = {};
 /* mongodb connection configuration */
 let noSqlDbConfig = {
   url: process.env.DB_URL || 'mongodb://localhost:27017/',
-  name: process.env.DB_NAME || 'prdxn',
+  name: process.env.DB_NAME || 'movie-api',
 };
 
 /* sql connection configuration */
